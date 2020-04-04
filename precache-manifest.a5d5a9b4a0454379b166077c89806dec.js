63507b24e82922a6dbcaaf0c96b216cc774b8819
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-04-04 11:11:51 +0900

    Updates

---
+++ precache-manifest.a5d5a9b4a0454379b166077c89806dec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c4426150741c1474399cbec715ad9bb",
+    "url": "/cra_test/index.html"
+  },
+  {
+    "revision": "8d006488cb7e9ce82e0b",
+    "url": "/cra_test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ee4d7b221a48b93f0a9a",
+    "url": "/cra_test/static/js/2.95284f81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cra_test/static/js/2.95284f81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d006488cb7e9ce82e0b",
+    "url": "/cra_test/static/js/main.6e4888a5.chunk.js"
+  },
+  {
+    "revision": "ad2b7a7e55cdbb4a47ee",
+    "url": "/cra_test/static/js/runtime-main.0467ead7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cra_test/static/media/logo.5d5d9eef.svg"
+  }
+]);
